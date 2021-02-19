86abbf7c76993cea6dec725b5c6e8d97fc4272de
Filipe Costa <filipi_costa@hotmail.com>
2021-02-19 12:29:12 -0300

    Update challenge-05.js

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -3,35 +3,43 @@ Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
 // ?
+var myArray = [1,'2',null,{},function(){}]
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
-
+function myFunction(arr){
+    return arr
+}
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
-
+myFunction(myArray)[1]
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
 segundo, um número. A função deve retornar o valor de um índice do array que foi passado
 no primeiro parâmetro. O índice usado para retornar o valor, deve ser o número passado no
 segundo parâmetro.
 */
-// ?
-
+function myFunction2(arr,index){
+  return arr[ index ] 
+}
+    
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-// ?
+var myArray2 = [1,'ok',true,[1,2],null]
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-// ?
+myFunction2(myArray2,0)
+myFunction2(myArray2,1)
+myFunction2(myArray2,2)
+myFunction2(myArray2,3)
+myFunction2(myArray2,4)
+
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -47,30 +55,57 @@ propriedades:
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
 */
-// ?
+
+function book (bookName) {
+    var allBooks = {
+        'Segredos de uma vida com sucesso' : {
+             quantidadePaginas : 388,
+             autor: 'Jonh Resing',
+             editora: 'Fundo de quintal'   
+         },
+        'Introducao ao PLSQL' : {
+             quantidadePaginas : 212,
+             autor: 'Jonh Wick',
+             editora: 'Fundo de quintal'   
+         },
+        'Introducao ao mundo NODEJS' : {
+             quantidadePaginas : 431,
+             autor: 'Jonh Baracho',
+             editora: 'Fundo de quintal'   
+         }
+    }
+    
+    if (!bookName) {
+       return allBooks
+    }
+    
+    return allBooks[ bookName ] 
+} 
+
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
 // ?
+book()
 
 /*
 Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
-// ?
-
+`"O livro Introducao ao PLSQL tem ${book('Introducao ao PLSQL').quantidadePaginas} páginas!" `
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
 a frase:
 "O autor do livro [NOME_DO_LIVRO] é [AUTOR]."
 */
-// ?
+`O autor do livro Introducao ao PLSQL é ${book('Introducao ao PLSQL').autor}.`
 
 /*
 Ainda com a função acima, imprima o nome da editora de um livro qualquer, usando
 a frase:
 "O livro [NOME_DO_LIVRO] foi publicado pela editora [NOME_DA_EDITORA]."
 */
-// ?
+`O livro ${bookName} foi publicado pela editora ${book(bookName).editora}.`
+"O livro Introducao ao PLSQL foi publicado pela editora Fundo de quintal."
